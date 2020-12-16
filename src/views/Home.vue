17df215d4414e76ca212b171f760c5c7f980004c
Nuri Akman <nuriakman@gmail.com>
2020-12-16 17:38:24 +0300

    Sadece VUE ile çalışan hali hazır

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="counter">{{counter}}</div>
+    <div class="buttons">
+      <button @click="decreaseCounter">-</button>
+      <button @click="increaseCounter">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    }
+  }
+}
+</script>
+
+<style>
+div {
+  margin-bottom: 20px;
+
+}
+.counter {
+  font-size: 80px;
+}
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0 20px;
+}
+</style>
